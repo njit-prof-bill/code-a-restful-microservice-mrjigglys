ee8920104305f955409b689c9e631a8befe846a9
MrJigglys <djtheman007@gmail.com>
2024-10-16 15:03:48 -0400

    Completed REST API microservice

--- src/index.js
+++ src/index.js
@@ -9,6 +9,62 @@ app.use(express.json());
 // Put your implementation here
 // If necessary to add imports, please do so in the section above
 
+//Data
+let users = [
+    {id: 0, name: "First Last", email: "name@site.st"}
+];
+
+var usercount = 1;
+//POST new user
+app.post('/users', (req, res) => {
+    const {name, email} = req.body;
+    if (!name || !email) {
+        return res.status(400).json({message: 'Missing name or email'});
+    }
+    const user = {id: usercount, name, email};
+    usercount++;
+    users.push(user);
+    res.status(201).json(user);
+});
+
+//GET user from id
+app.get('/users/:id', (req, res) => {
+    const {id} = req.params;
+    //Match ID to a user
+    const user = users.find((user) => user.id === parseInt(id));
+    if (!user) {
+        return res.status(404).json({message: 'User not found'});
+    }
+
+    res.json(user);
+});
+
+//PUT update user by id
+app.put('/users/:id', (req, res) => {
+    const {id} = req.params;
+    const {name, email} = req.body;
+    if (!name || !email) {
+        return res.status(400).json({message: 'Missing name or email'});
+    }
+    const user = users.find((user) => user.id === parseInt(id));
+    if (!user) {
+
+        return res.status(404).json({ message: 'User not found' });
+    
+      }
+    user.name = name;
+    user.email = email;
+
+    res.json(user);
+});
+
+//DELETE user by id
+app.delete('/users/:id', (req, res) => {
+    const {id} = req.params;
+    users = users.filter((user) => user.id !== parseInt(id));
+    res.sendStatus(204);
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
